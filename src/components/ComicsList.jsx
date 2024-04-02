8c2d9e7b5c64010b714b97d744e3f24b0c9ac569
Sonja Fallstrom <jawnlemon@gmail.com>
2024-04-02 14:44:06 +0300

    changed card direction horizontal in comiclist

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -6,25 +6,25 @@ function ComicsList({ comics, showbtn }) {
     return (
         <>
             <Typography variant="h2">Sarjakuvalista</Typography>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} >
                 {
                     comics.map(comic => {
                         return (
-                            <Grid item key={comic.id}>
-                                <Card sx={{ width: 320 }}>
-                                    <CardHeader title={`${comic.name} ${comic.additionalName}`} />
+                            <Grid item key={comic.id} xs={10} >
+                                <Card sx={{ display: "flex" }}>
                                     {
                                         comic.image !== "" ?
-                                            <CardMedia sx={{ height: 220 }} component="img" image={`src/images/${comic.image}`} alt="Sarjakuvan kansikuva" />
-                                            : <CardContent sx={{ textAlign: "center", height: 120, backgroundColor: "grey.300" }}><Typography >Ei kuvaa</Typography></CardContent>
+                                            <CardMedia sx={{ height: 220, width: 320, }} component="img" image={`src/images/${comic.image}`} alt="Sarjakuvan kansikuva" />
+                                            : <CardContent sx={{ textAlign: "center", height: 220, width: 200, backgroundColor: "grey.300" }}><Typography >Ei kuvaa</Typography></CardContent>
                                     }
-                                    <CardContent>
+                                    <CardContent sx={{ flexGrow: 8 }}>
+                                        <CardHeader title={`${comic.name} ${comic.additionalName}`} />
                                         <Typography>Kirjoittaja: {comic.writer}</Typography>
                                         <Typography>Piirtäjä: {comic.artist}</Typography>
                                         <Typography>Julkaisuvuosi: {comic.publicationYear}</Typography>
                                         <Typography>Luettu: {comic.dateRead}</Typography>
                                     </CardContent>
-                                    <CardActions>
+                                    <CardActions sx={{}}>
                                         <Button color="primary" onClick={() => showbtn(comic.id)}>Näytä lisätietoja</Button>
                                     </CardActions>
                                 </Card>
