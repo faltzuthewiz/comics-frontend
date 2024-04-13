22ab6201531c47648a0ef489136d8bd103c3c46b
Sonja Fallstrom <jawnlemon@gmail.com>
2024-04-13 10:45:04 +0300

    started infopage, added a new theme

---
+++ src/components/InfoPage.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material"
+
+function InfoPage() {
+    return (
+        <Box sx={{ marginTop: "120px" }}>
+            <Typography variant="h2">Tietoa sivustosta</Typography>
+            <Typography>Esittely...</Typography>
+        </Box>
+    )
+}
+
+export default InfoPage
