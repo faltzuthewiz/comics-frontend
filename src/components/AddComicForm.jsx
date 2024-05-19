4e737cf2e6d81e57382a55894fcfb703e2541f74
Sonja Fallstrom <jawnlemon@gmail.com>
2024-05-19 14:36:48 +0300

    added validation for adding and editing a comic

--- src/components/AddComicForm.jsx
+++ src/components/AddComicForm.jsx
@@ -35,7 +35,7 @@ function AddComicForm() {
     })
 
     const [showBox, setShowBox] = useState(false)
-
+    const [errors, setErrors] = useState({})
     const [text, setText] = useState("")
 
     const change = (e) => {
@@ -49,6 +49,7 @@ function AddComicForm() {
 
         setText("")
         setShowBox(false)
+        setErrors({ ...errors, [name]: '' })
     }
 
     const changeCheck = (e) => {
@@ -59,6 +60,7 @@ function AddComicForm() {
 
         setText("")
         setShowBox(false)
+        setErrors({ ...errors, [e.target.name]: '' })
     }
 
     const changeDate = (e) => {
@@ -69,6 +71,7 @@ function AddComicForm() {
 
         setText("")
         setShowBox(false)
+        setErrors({ ...errors, dateRead: '' })
     }
 
     const changeLanguage = (e) => {
@@ -79,6 +82,7 @@ function AddComicForm() {
 
         setText("")
         setShowBox(false)
+        setErrors({ ...errors, language: '' })
     }
 
     const changeImage = (e) => {
@@ -89,6 +93,7 @@ function AddComicForm() {
 
         setText("")
         setShowBox(false)
+        setErrors({ ...errors, image: '' })
     }
 
     let imageName = ''
@@ -96,53 +101,75 @@ function AddComicForm() {
         imageName = comic.image.name
     }
 
+    const validate = () => {
+        let tempErrors = {};
+        tempErrors.name = comic.name ? "" : "Sarjakuvan nimi on pakollinen";
+        tempErrors.artist = comic.artist ? "" : "Piirtäjä on pakollinen";
+        tempErrors.writer = comic.writer ? "" : "Käsikirjoittaja on pakollinen";
+        tempErrors.details = comic.details ? "" : "Kuvaus on pakollinen";
+        tempErrors.pages = comic.pages && !isNaN(comic.pages) ? "" : "Sivumäärä on pakollinen ja sen pitää olla numero";
+        tempErrors.publicationYear = comic.publicationYear && !isNaN(comic.publicationYear) ? "" : "Julkaisuvuosi on pakollinen ja sen pitää olla numero";
+        tempErrors.ISBN = comic.ISBN.match(/^[0-9]*$/) ? "" : "ISBN-numero voi sisältää vain numeroita. Jätä väliviivat pois.";
+        tempErrors.language = comic.language ? "" : "Kieli on pakollinen";
+        tempErrors.ownThoughts = comic.ownThoughts ? "" : "Oman kommentin kirjoittaminen on pakollista";
+
+        setErrors(tempErrors);
+        return Object.values(tempErrors).every(x => x === "");
+    }
+
     const addNewComic = async () => {
-        const formData = new FormData()
-        formData.append('name', comic.name)
-        formData.append('additionalName', comic.additionalName)
-        formData.append('translation', comic.translation)
-        formData.append('originalName', comic.originalName)
-        formData.append('artist', comic.artist)
-        formData.append('writer', comic.writer)
-        formData.append('details', comic.details)
-        formData.append('pages', comic.pages)
-        formData.append('publicationYear', comic.publicationYear)
-        formData.append('ISBN', comic.ISBN)
-        formData.append('selfPublished', comic.selfPublished)
-        formData.append('publisher', comic.publisher)
-        formData.append('language', comic.language)
+        if (validate()) {
+            const formData = new FormData()
+            formData.append('name', comic.name)
+            formData.append('additionalName', comic.additionalName)
+            formData.append('translation', comic.translation)
+            formData.append('originalName', comic.originalName)
+            formData.append('artist', comic.artist)
+            formData.append('writer', comic.writer)
+            formData.append('details', comic.details)
+            formData.append('pages', comic.pages)
+            formData.append('publicationYear', comic.publicationYear)
+            formData.append('ISBN', comic.ISBN)
+            formData.append('selfPublished', comic.selfPublished)
+            formData.append('publisher', comic.publisher)
+            formData.append('language', comic.language)
 
-        let date = comic.dateRead.getFullYear() + "-" + (comic.dateRead.getMonth() + 1) + "-" + comic.dateRead.getDate()
-        formData.append('dateRead', date)
-        formData.append('image', comic.image)
-        formData.append('ownThoughts', comic.ownThoughts)
+            let date = comic.dateRead.getFullYear() + "-" + (comic.dateRead.getMonth() + 1) + "-" + comic.dateRead.getDate()
+            formData.append('dateRead', date)
+            formData.append('image', comic.image)
+            formData.append('ownThoughts', comic.ownThoughts)
 
-        try {
-            const response = await addComic(formData)
-            setComic({
-                name: "",
-                additionalName: "",
-                translation: false,
-                originalName: "",
-                artist: "",
-                writer: "",
-                details: "",
-                pages: "",
-                publicationYear: 1900,
-                ISBN: "",
-                selfPublished: false,
-                publisher: "",
-                language: "Finnish",
-                dateRead: new Date(),
-                image: "",
-                ownThoughts: "",
-            })
-            setText('Uusi sarjakuva lisätty!')
-            setShowBox(true)
-        } catch (error) {
-            setText('Uuden sarjakuvan lisääminen ei onnistunut!')
+            try {
+                const response = await addComic(formData)
+                setComic({
+                    name: "",
+                    additionalName: "",
+                    translation: false,
+                    originalName: "",
+                    artist: "",
+                    writer: "",
+                    details: "",
+                    pages: "",
+                    publicationYear: 1900,
+                    ISBN: "",
+                    selfPublished: false,
+                    publisher: "",
+                    language: "Finnish",
+                    dateRead: new Date(),
+                    image: "",
+                    ownThoughts: "",
+                })
+                setText('Uusi sarjakuva lisätty!')
+                setShowBox(true)
+            } catch (error) {
+                setText('Uuden sarjakuvan lisääminen ei onnistunut!')
+                setShowBox(false)
+            }
+        } else {
+            setText("Tarkista virheet!")
             setShowBox(false)
         }
+
     }
 
     const emptyChanges = () => {
@@ -164,23 +191,30 @@ function AddComicForm() {
             image: "",
             ownThoughts: "",
         })
+
+        setErrors({})
+        setText('')
+        setShowBox(false)
     }
 
     return (
         <>
-            <Paper sx={{ maxWidth: 1200, margin: "auto", marginTop: "120px", marginBottom: "40px", }}>
+            <Paper sx={{ maxWidth: 1000, margin: "auto", marginTop: "120px", marginBottom: "40px", }}>
                 <Typography variant="h2">Lisää uusi sarjakuva</Typography>
 
                 <Box component='form' autoComplete='off' sx={{ '& .MuiTextField-root': { marginBottom: 2 } }}>
-                    <TextField label='Sarjakuvan nimi' name="name" value={comic.name} onChange={change} fullWidth autoFocus required />
+                    <TextField label='Sarjakuvan nimi' name="name" value={comic.name} onChange={change} fullWidth autoFocus required helperText={errors.name ? errors.name : "Kirjoita teoksen päänimi"}
+                        error={Boolean(errors.name)} />
                     <br />
-                    <TextField label='Teoksen lisänimi' name="additionalName" value={comic.additionalName} onChange={change} fullWidth />
+                    <TextField label='Teoksen lisänimi' name="additionalName" value={comic.additionalName} onChange={change} fullWidth helperText="Kirjoita teoksen mahdollinen lisänimi" />
                     <br />
-                    <TextField label='Piirtäjä' name="artist" value={comic.artist} onChange={change} fullWidth required />
+                    <TextField label='Piirtäjä' name="artist" value={comic.artist} onChange={change} fullWidth required helperText={errors.artist ? errors.artist : ""}
+                        error={Boolean(errors.artist)} />
                     <br />
-                    <TextField label='Käsikirjoittaja' name="writer" value={comic.writer} onChange={change} fullWidth required />
+                    <TextField label='Käsikirjoittaja' name="writer" value={comic.writer} onChange={change} fullWidth required helperText={errors.writer ? errors.writer : ""}
+                        error={Boolean(errors.writer)} />
                     <br />
-                    <FormControlLabel labelPlacement="start" label='Onko teos käännös?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.translation} onChange={changeCheck} name="translation" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
+                    <FormControlLabel labelPlacement="start" label='Onko teos käännös toisesta kielestä?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.translation} onChange={changeCheck} name="translation" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
                     <br />
                     {comic.translation == true && (
                         <>
@@ -188,15 +222,19 @@ function AddComicForm() {
                             <br />
                         </>
                     )}
-                    <TextField label='Kuvaus' name="details" value={comic.details} onChange={change} multiline rows='4' fullWidth required />
+                    <TextField label='Kuvaus' name="details" value={comic.details} onChange={change} multiline rows='4' fullWidth required helperText={errors.details ? errors.details : ""}
+                        error={Boolean(errors.details)} />
                     <br />
-                    <TextField label='Sivumäärä' name="pages" value={comic.pages} onChange={change} fullWidth required />
+                    <TextField label='Sivumäärä' name="pages" value={comic.pages} onChange={change} fullWidth required helperText={errors.pages ? errors.pages : "Kirjoita sivumäärä numeroina, esim. 54"}
+                        error={Boolean(errors.pages)} />
                     <br />
-                    <TextField label='Julkaisuvuosi' name="publicationYear" value={comic.publicationYear} onChange={change} fullWidth required />
+                    <TextField label='Julkaisuvuosi' name="publicationYear" value={comic.publicationYear} onChange={change} fullWidth required helperText={errors.publicationYear ? errors.publicationYear : "Kirjoita julkaisuvuosi numeroina, esim. 2024"}
+                        error={Boolean(errors.publicationYear)} />
                     <br />
-                    <TextField label='ISBN' name="ISBN" value={comic.ISBN} onChange={change} fullWidth />
+                    <TextField label='ISBN' name="ISBN" value={comic.ISBN} onChange={change} fullWidth helperText={errors.ISBN ? errors.ISBN : "Kirjoita ISBN-numero yhteen, ilman väliviivoja"}
+                        error={Boolean(errors.ISBN)} />
                     <br />
-                    <FormControlLabel labelPlacement="start" label='Onko teos omakustanne?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.selfPublished} onChange={changeCheck} name="selfPublished" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
+                    <FormControlLabel labelPlacement="start" label='Onko teos omakustanne (Esim. tekijän itsensä myymät pienlehdet, zinet)?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.selfPublished} onChange={changeCheck} name="selfPublished" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
                     <br />
                     {comic.selfPublished == false && (
                         <>
@@ -224,7 +262,8 @@ function AddComicForm() {
                     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={fi}>
                         <DesktopDatePicker sx={{ width: '100%' }} onChange={changeDate} name="dateRead" value={comic.dateRead} />
                     </LocalizationProvider>
-                    <TextField label='Omat ajatukset' name="ownThoughts" value={comic.ownThoughts} onChange={change} multiline rows='4' fullWidth required />
+                    <TextField label='Omat ajatukset' name="ownThoughts" value={comic.ownThoughts} onChange={change} multiline rows='4' fullWidth required helperText={errors.ownThoughts ? errors.ownThoughts : ""}
+                        error={Boolean(errors.ownThoughts)} />
                     <br />
                     <input accept="image/*" name="image" id="image" type="file" onChange={changeImage} hidden />
                     <InputLabel htmlFor="image">
--- src/components/EditComicForm.jsx
+++ src/components/EditComicForm.jsx
@@ -39,6 +39,7 @@ function EditComicForm() {
     })
 
     const [text, setText] = useState("")
+    const [errors, setErrors] = useState({})
 
     useEffect(() => {
         getOneComic(id)
@@ -75,6 +76,7 @@ function EditComicForm() {
         })
 
         setText("")
+        setErrors({ ...errors, [name]: '' })
     }
 
     const changeCheck = (e) => {
@@ -84,6 +86,7 @@ function EditComicForm() {
         })
 
         setText("")
+        setErrors({ ...errors, [e.target.name]: '' })
     }
 
     const changeDate = (e) => {
@@ -95,6 +98,7 @@ function EditComicForm() {
         console.log(comic)
 
         setText("")
+        setErrors({ ...errors, dateRead: '' })
     }
 
     const changeLanguage = (e) => {
@@ -104,6 +108,7 @@ function EditComicForm() {
         })
 
         setText("")
+        setErrors({ ...errors, language: '' })
     }
 
     const changeImage = (e) => {
@@ -113,6 +118,7 @@ function EditComicForm() {
         })
 
         setText("")
+        setErrors({ ...errors, image: '' })
     }
 
     let imageName = ''
@@ -120,51 +126,76 @@ function EditComicForm() {
         imageName = comic.image.name
     }
 
+    const validate = () => {
+        let tempErrors = {};
+        tempErrors.name = comic.name ? "" : "Sarjakuvan nimi on pakollinen";
+        tempErrors.artist = comic.artist ? "" : "Piirtäjä on pakollinen";
+        tempErrors.writer = comic.writer ? "" : "Käsikirjoittaja on pakollinen";
+        tempErrors.details = comic.details ? "" : "Kuvaus on pakollinen";
+        tempErrors.pages = comic.pages && !isNaN(comic.pages) ? "" : "Sivumäärä on pakollinen ja sen pitää olla numero";
+        tempErrors.publicationYear = comic.publicationYear && !isNaN(comic.publicationYear) ? "" : "Julkaisuvuosi on pakollinen ja sen pitää olla numero";
+        tempErrors.ISBN = comic.ISBN && comic.ISBN.toString().match(/^[0-9]*$/) ? "" : "ISBN-numero voi sisältää vain numeroita. Jätä väliviivat pois.";
+        tempErrors.language = comic.language ? "" : "Kieli on pakollinen";
+        tempErrors.ownThoughts = comic.ownThoughts ? "" : "Oman kommentin kirjoittaminen on pakollista";
+
+        setErrors(tempErrors);
+        return Object.values(tempErrors).every(x => x === "");
+    }
+
     const submitEditedComic = async () => {
-        const comicId = comic.id
 
-        const formData = new FormData()
-        formData.append('name', comic.name)
-        formData.append('additionalName', comic.additionalName)
-        formData.append('translation', comic.translation)
-        formData.append('originalName', comic.originalName)
-        formData.append('artist', comic.artist)
-        formData.append('writer', comic.writer)
-        formData.append('details', comic.details)
-        formData.append('pages', comic.pages)
-        formData.append('publicationYear', comic.publicationYear)
-        formData.append('ISBN', comic.ISBN)
-        formData.append('selfPublished', comic.selfPublished)
-        formData.append('publisher', comic.publisher)
-        formData.append('language', comic.language)
-        formData.append('dateRead', comic.dateRead)
-        formData.append('image', comic.image)
-        formData.append('ownThoughts', comic.ownThoughts)
+        if (validate()) {
+            const comicId = comic.id
 
-        try {
-            const response = await editComic(comicId, formData)
-            setText('Sarjakuvaa muokattu!')
-        } catch (error) {
-            setText('Virhe! Sarjakuvan muokkaaminen ei onnistunut.')
+            const formData = new FormData()
+            formData.append('name', comic.name)
+            formData.append('additionalName', comic.additionalName)
+            formData.append('translation', comic.translation)
+            formData.append('originalName', comic.originalName)
+            formData.append('artist', comic.artist)
+            formData.append('writer', comic.writer)
+            formData.append('details', comic.details)
+            formData.append('pages', comic.pages)
+            formData.append('publicationYear', comic.publicationYear)
+            formData.append('ISBN', comic.ISBN)
+            formData.append('selfPublished', comic.selfPublished)
+            formData.append('publisher', comic.publisher)
+            formData.append('language', comic.language)
+            formData.append('dateRead', comic.dateRead)
+            formData.append('image', comic.image)
+            formData.append('ownThoughts', comic.ownThoughts)
+
+            try {
+                const response = await editComic(comicId, formData)
+                setText('Sarjakuvaa muokattu!')
+            } catch (error) {
+                setText('Virhe! Sarjakuvan muokkaaminen ei onnistunut.')
+            }
+        } else {
+            setText("Tarkista virheet!")
         }
+
     }
 
 
     return (
         <>
-            <Paper sx={{ maxWidth: 1200, marginTop: "120px", marginLeft: "10%", marginBottom: "40px" }}>
+            <Paper sx={{ maxWidth: 1000, margin: "auto", marginTop: "120px", marginBottom: "40px" }}>
                 <Typography variant="h2">Muokkaa sarjakuvaa</Typography>
 
                 <Box component='form' autoComplete='off' sx={{ '& .MuiTextField-root': { marginBottom: 2 } }}>
-                    <TextField label='Sarjakuvan nimi' name="name" value={comic.name} onChange={change} fullWidth autoFocus required />
+                    <TextField label='Sarjakuvan nimi' name="name" value={comic.name} onChange={change} fullWidth autoFocus required helperText={errors.name ? errors.name : "Kirjoita teoksen päänimi"}
+                        error={Boolean(errors.name)} />
                     <br />
-                    <TextField label='Teoksen lisänimi' name="additionalName" value={comic.additionalName} onChange={change} fullWidth />
+                    <TextField label='Teoksen lisänimi' name="additionalName" value={comic.additionalName} onChange={change} fullWidth helperText="Kirjoita teoksen mahdollinen lisänimi" />
                     <br />
-                    <TextField label='Piirtäjä' name="artist" value={comic.artist} onChange={change} fullWidth required />
+                    <TextField label='Piirtäjä' name="artist" value={comic.artist} onChange={change} fullWidth required helperText={errors.artist ? errors.artist : ""}
+                        error={Boolean(errors.artist)} />
                     <br />
-                    <TextField label='Käsikirjoittaja' name="writer" value={comic.writer} onChange={change} fullWidth required />
+                    <TextField label='Käsikirjoittaja' name="writer" value={comic.writer} onChange={change} fullWidth required helperText={errors.writer ? errors.writer : ""}
+                        error={Boolean(errors.writer)} />
                     <br />
-                    <FormControlLabel labelPlacement="start" label='Onko teos käännös?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.translation} onChange={changeCheck} name="translation" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
+                    <FormControlLabel labelPlacement="start" label='Onko teos käännös toisesta kielestä?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.translation} onChange={changeCheck} name="translation" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
                     <br />
                     {comic.translation == true && (
                         <>
@@ -172,15 +203,19 @@ function EditComicForm() {
                             <br />
                         </>
                     )}
-                    <TextField label='Kuvaus' name="details" value={comic.details} onChange={change} multiline rows='4' fullWidth required />
+                    <TextField label='Kuvaus' name="details" value={comic.details} onChange={change} multiline rows='4' fullWidth required helperText={errors.details ? errors.details : ""}
+                        error={Boolean(errors.details)} />
                     <br />
-                    <TextField label='Sivumäärä' name="pages" value={comic.pages} onChange={change} fullWidth required />
+                    <TextField label='Sivumäärä' name="pages" value={comic.pages} onChange={change} fullWidth required helperText={errors.pages ? errors.pages : "Kirjoita sivumäärä numeroina, esim. 54"}
+                        error={Boolean(errors.pages)} />
                     <br />
-                    <TextField label='Julkaisuvuosi' name="publicationYear" value={comic.publicationYear} onChange={change} fullWidth required />
+                    <TextField label='Julkaisuvuosi' name="publicationYear" value={comic.publicationYear} onChange={change} fullWidth required helperText={errors.publicationYear ? errors.publicationYear : "Kirjoita julkaisuvuosi numeroina, esim. 2024"}
+                        error={Boolean(errors.publicationYear)} />
                     <br />
-                    <TextField label='ISBN' name="ISBN" value={comic.ISBN} onChange={change} fullWidth />
+                    <TextField label='ISBN' name="ISBN" value={comic.ISBN} onChange={change} fullWidth helperText={errors.ISBN ? errors.ISBN : "Kirjoita ISBN-numero yhteen, ilman väliviivoja"}
+                        error={Boolean(errors.ISBN)} />
                     <br />
-                    <FormControlLabel labelPlacement="start" label='Onko teos omakustanne?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.selfPublished} onChange={changeCheck} name="selfPublished" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
+                    <FormControlLabel labelPlacement="start" label='Onko teos omakustanne (Esim. tekijän itsensä myymät pienlehdet, zinet)?' control={<><Typography>Kyllä</Typography><Switch color="primary" checked={comic.selfPublished} onChange={changeCheck} name="selfPublished" /><Typography sx={{ marginLeft: "10px" }}>Ei</Typography></>} />
                     <br />
                     {comic.selfPublished == false && (
                         <>
@@ -208,7 +243,8 @@ function EditComicForm() {
                     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={fi}>
                         <DesktopDatePicker sx={{ width: '100%' }} onChange={changeDate} name="dateRead" value={comic.dateRead} />
                     </LocalizationProvider>
-                    <TextField label='Omat ajatukset' name="ownThoughts" value={comic.ownThoughts} onChange={change} multiline rows='4' fullWidth required />
+                    <TextField label='Omat ajatukset' name="ownThoughts" value={comic.ownThoughts} onChange={change} multiline rows='4' fullWidth required helperText={errors.ownThoughts ? errors.ownThoughts : ""}
+                        error={Boolean(errors.ownThoughts)} />
                     <br />
                     <input accept="image/*" name="image" id="image" type="file" onChange={changeImage} hidden />
                     <InputLabel htmlFor="image">
